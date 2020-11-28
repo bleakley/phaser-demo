0b18a0787b05c8cc14566f41b27f38647ffd5786
Brian Bleakley <brianbleakley@gmail.com>
2020-11-28 10:35:28 -0700

    clean up

--- server/index.js
+++ server/index.js
@@ -18,27 +18,29 @@ app.get('/', function (req, res) {
 function setupAuthoritativePhaser() {
   JSDOM.fromFile(path.join(__dirname, 'authoritative_server/index.html'), {
     // To run the scripts in the html file
-    runScripts: "dangerously",
+    runScripts: 'dangerously',
     // Also load supported external resources
-    resources: "usable",
+    resources: 'usable',
     // So requestAnimatinFrame events fire
     pretendToBeVisual: true
-  }).then((dom) => {
-    dom.window.URL.createObjectURL = (blob) => {
-      if (blob){
-        return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
-      }
-    };
-    dom.window.URL.revokeObjectURL = (objectURL) => {};
-    dom.window.gameLoaded = () => {
-      server.listen(8082, function () {
-        console.log(`Listening on ${server.address().port}`);
-      });
-    };
-    dom.window.io = io;
-  }).catch((error) => {
-    console.log(error.message);
-  });
+  })
+    .then((dom) => {
+      dom.window.URL.createObjectURL = (blob) => {
+        if (blob) {
+          return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+        }
+      };
+      dom.window.URL.revokeObjectURL = (objectURL) => {};
+      dom.window.gameLoaded = () => {
+        server.listen(8082, function () {
+          console.log(`Listening on ${server.address().port}`);
+        });
+      };
+      dom.window.io = io;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
 
 setupAuthoritativePhaser();
